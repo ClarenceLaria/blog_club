1940b0fcc11bd6c32f2f8601d8de51ac3311ccbe
ClarenceLaria <clarencelaria@gmail.com>
2025-02-03 13:23:25 +0300

    Animated smooth indicator updates

--- lib/on_boarding.dart
+++ lib/on_boarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
@@ -30,38 +31,73 @@ class OnBoarding extends StatelessWidget {
                       child: Image(
                         image: const AssetImage('assets/images/on_boarding_images/reading.jpg'),
                         height: MediaQuery.of(context).size.height * 0.5,
-                        width: MediaQuery.of(context).size.width * 0.9,
+                        width: MediaQuery.of(context).size.width * 0.8,
                         fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
                 Container(
+                  width: double.infinity,
                   height: MediaQuery.of(context).size.height * 0.3,
                   padding: const EdgeInsets.symmetric(vertical: 16),
                   decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(24), topRight: Radius.circular(24)),
                     color: Colors.white,
                   ),
-                  child: const Column(
-                    children: [
-                      Text(
-                        'Welcome to Blog Club',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.8,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Welcome to Blog Club',
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                          textAlign: TextAlign.start,
                         ),
-                      ),
-                      Text(
-                        'Discover a world of curated content tailored to your interests.',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black,
+                        const SizedBox(height: 20),
+                        const Text(
+                          'Discover a world of curated content tailored to your interests.',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AnimatedSmoothIndicator(
+                              activeIndex: 3,
+                              count: 4,
+                              effect: ExpandingDotsEffect(
+                                dotWidth: 10,
+                                dotHeight: 10,
+                                activeDotColor: const Color.fromARGB(255,56, 106, 237),
+                                dotColor: const Color.fromARGB(255, 56, 106, 237).withOpacity(0.5),
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {}, 
+                              style: IconButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 30.0),
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                                ),
+                                backgroundColor: const Color.fromARGB(255,56, 106, 237),
+                              ),
+                              icon: const Icon(Icons.arrow_forward_outlined, size: 30.0,), 
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
