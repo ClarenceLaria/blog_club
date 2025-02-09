d173d6a3229b79f13d27e9d79c544d4cdcf05602
ClarenceLaria <clarencelaria@gmail.com>
2025-02-09 07:32:31 +0300

    Flutter quil configurations

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -19,6 +19,8 @@ class _AddPageState extends State<AddPage> {
     DropdownMenuItem(value: 'Category3', child: Text('Category3')),
   ];
 
+  final QuillController _controller = QuillController.basic();
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -47,27 +49,85 @@ class _AddPageState extends State<AddPage> {
                     ),
                   ],
                 ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Title',
-                    hintText: 'Enter a Blog Title',
-                    // border: OutlineInputBorder(),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Title',
+                          hintText: 'Enter a Blog Title',
+                          // border: OutlineInputBorder(),
+                        ),
+                        maxLength: 100,
+                        maxLines: null,
+                      ),
+                      // const SizedBox(height: 10),
+                      // DropdownButtonFormField(
+                      //   items: items, 
+                      //   onChanged: (value) {},
+                      //   decoration: const InputDecoration(
+                      //     labelText: 'Category',
+                      //     hintText: 'Select a Category',
+                      //     // border: OutlineInputBorder(),
+                      //   ),
+                      // ),
+                      const SizedBox(height: 10),
+                      QuillSimpleToolbar(
+                        controller: _controller,
+                        configurations: const QuillSimpleToolbarConfigurations(
+                          toolbarIconAlignment: WrapAlignment.start,
+                          toolbarIconCrossAlignment: WrapCrossAlignment.start,
+                          toolbarSectionSpacing: kToolbarSectionSpacing/20,
+                          multiRowsDisplay: false,
+                          showDividers: false,
+                          showFontFamily: false,
+                          showFontSize: false,
+                          showBoldButton: true,
+                          showItalicButton: true,
+                          showSmallButton: false,
+                          showUnderLineButton: false,
+                          showLineHeightButton: false,
+                          showStrikeThrough: false,
+                          showInlineCode: false,
+                          showColorButton: false,
+                          showBackgroundColorButton: false,
+                          showClearFormat: false,
+                          showAlignmentButtons: true,
+                          showLeftAlignment: true,
+                          showCenterAlignment: false,
+                          showRightAlignment: false,
+                          showJustifyAlignment: false,
+                          showHeaderStyle: true,
+                          showListNumbers: true,
+                          showListBullets: true,
+                          showListCheck: false,
+                          showCodeBlock: false,
+                          showQuote: false,
+                          showIndent: false,
+                          showLink: true,
+                          showUndo: true,
+                          showRedo: true,
+                          showDirection: false,
+                          showSearchButton: false,
+                          showSubscript: false,
+                          showSuperscript: false,
+                          showClipboardCut: true,
+                          showClipboardCopy: true,
+                          showClipboardPaste: true,
+                        ),
+                      ),
+                      Expanded(
+                        child: QuillEditor.basic(
+                          controller: _controller,
+                          // focusNode: FocusNode(),
+                          configurations: const QuillEditorConfigurations(),
+                        ),
+                      )
+                    ],
                   ),
-                  maxLength: 100,
-                  maxLines: null,
-                ),
-                const SizedBox(height: 20),
-                DropdownButtonFormField(
-                  items: items, 
-                  onChanged: (value) {},
-                  decoration: const InputDecoration(
-                    labelText: 'Category',
-                    hintText: 'Select a Category',
-                    // border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20),
+                )
               ],
             ),
           ),
